d9a0c674b1634abf362f13888d68ffd08f6ff61b
alexanderdev. <alexandrunae234@gmail.com>
2026-06-29 21:30:27 +0300

    Add files via upload

--- api/getData.js
+++ api/getData.js
@@ -6,7 +6,8 @@ export default async function handler(req, res) {
     if (data) {
       res.status(200).json(data);
     } else {
-      res.status(404).json({ error: 'No data found' });
+      // Empty database should not trigger an error, just return empty state
+      res.status(200).json({});
     }
   } catch (error) {
     console.error(error);
